3d35174181775c35958da7e9a8373a9f650a874d
Vicenthresh <vsolis199@gmail.com>
2021-08-03 00:36:06 -0400

    models Comercio, Categoria, Menu

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import *
+
+admin.site.register(Comercio)
+admin.site.register(Categoria)
+admin.site.register(Menu)
--- app/models.py
+++ app/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
 # Create your models here.
+class Comercio(models.Model):
+    nombre = models.CharField(max_length=100, null=True)
+    descripcion = models.CharField(max_length=500, null=True)
+    direccion = models.CharField(max_length=100, null=True)
+    is_active = models.BooleanField(default=True)
+
+class Categoria(models.Model):
+    nombre =(
+                ('Fritura', 'Fritura'),
+                ('Fruta', 'Fruta'),
+                ('Vegana', 'Vegana'),
+            )
+
+    def __str__(self):
+        return self.name
+
+class Menu(models.Model):
+    comercio = models.ForeignKey(Comercio, null=True, on_delete=models.SET_NULL)
+    
+    titulo = models.CharField(max_length=100, null=True)
+    precio = models.PositiveIntegerField()
+    descripcion = models.CharField(max_length=500, null=True)
+    calificacion_media = models.FloatField(default=0)
+    is_active = models.BooleanField(default=True)
+
+    categoria = models.ManyToManyField(Categoria)
